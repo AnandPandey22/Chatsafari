569cc713c5963612e01a3b09aa9cd45ab8517c80
Anand Pandey <ap3832576@gmail.com>
2025-03-14 15:00:33 +0530

    Update socket.ts

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,8 +1,8 @@
-import { io } from "socket.io-client";
+import { Socket, io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
-export const socket = io(SOCKET_URL, {
+export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
   reconnection: true,
   withCredentials: true,
