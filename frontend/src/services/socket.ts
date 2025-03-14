4c2b6b61e15a4c7637a5edd9845e644954182356
Anand Pandey <ap3832576@gmail.com>
2025-03-14 15:07:03 +0530

    Update socket.ts

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,4 +1,5 @@
-import { Socket, io } from "socket.io-client";
+import { Socket } from "socket.io-client";
+import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
