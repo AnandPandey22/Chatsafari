a29cd710d2a0e5d9135d00acc70765146647106d
Anand Pandey <ap3832576@gmail.com>
2025-04-13 15:13:53 +0530

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -61,6 +61,7 @@ const Login: React.FC = () => {
       <Helmet>
         <link rel="canonical" href="https://chatsafari.com/" />
       </Helmet>
+      
       {/* Fixed Header */}
       <header className="bg-white border-b border-gray-200 shadow-sm fixed top-0 left-0 right-0 z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
