85bd12d53a59dd43c58564d0572748537acd7836
Anand Pandey <ap3832576@gmail.com>
2025-04-15 15:53:38 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -2976,9 +2976,9 @@ const Blogs: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-center items-center h-16">
             <Link to="/" className="flex items-center">
-              <h1 className="text-2xl font-bold text-violet-600" style={{ fontFamily: 'Pacifico, cursive' }}>
+              <span className="text-2xl font-bold text-violet-600" style={{ fontFamily: 'Pacifico, cursive' }}>
                 ChatSafari
-              </h1>
+              </span>
             </Link>
           </div>
         </div>
