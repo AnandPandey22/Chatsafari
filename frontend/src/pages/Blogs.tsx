1ad6af3fe0692477d34896362040b33d544f4c51
Anand Pandey <ap3832576@gmail.com>
2025-04-24 14:12:50 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -97,7 +97,7 @@ In a digital world craving authenticity, Anand Pandey is a breath of fresh air. 
     thumbnail: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?w=500&q=80",
     excerpt: "Great ways to start and maintain engaging conversations.",
     date: "2024-03-12",
-    content: '
+    content: (
 
     ## Key Features of ChatSafari
 
@@ -107,7 +107,7 @@ In a digital world craving authenticity, Anand Pandey is a breath of fresh air. 
 - **Real-time active users list** – See who's online and start chatting instantly.
 - **Fun features** like message reactions, media sharing, typing indicators, and gender-based avatars.
 - **Privacy first** – No data storage. Chats vanish once the session ends.
-    '
+    )
    
   },
   {
