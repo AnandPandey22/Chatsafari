4ecd579fecb2abc0ace185a3b843ffcc3da5c2cc
Anand Pandey <ap3832576@gmail.com>
2025-04-30 21:28:23 +0530

    Update Blogs.tsx

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -173,7 +173,7 @@ Join the growing community of Chatsafari users today and experience the future o
   },
   {
     id: 3,
-    title: "Talk to Strangers On Chatsafari",
+    title: "Talk to Strangers",
     slug: "talk-to-strangers-online",
     thumbnail: "https://images.unsplash.com/photo-1577563908411-5077b6dc7624?w=500&q=80",
     excerpt: "Talk to strangers online free Chat Room on chatsafari without registration, USA Chatroooms, Free Online chat, Chat with strangers",
